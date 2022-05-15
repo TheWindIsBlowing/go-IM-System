0a38e3a0e55efe685f08a835d1c9234ea6b5750f
wind <1476212137@qq.com>
2022-05-15 17:36:18 +0800

    广播用户消息

--- server.go
+++ server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net"
 	"sync"
 )
@@ -63,6 +64,29 @@ func (server *Server) Handler(conn net.Conn) {
 
 	// 广播当前用户上线消息
 	server.BroadCast(user, "online")
+
+	// 接收客户端发送的消息
+	go func() {
+		buf := make([]byte, 4096)
+		for {
+			n, err := conn.Read(buf)
+			if n == 0 {
+				server.BroadCast(user, "offline")
+				return
+			}
+
+			if err != nil && err != io.EOF {
+				fmt.Println("Conn Read err: ", err)
+				return
+			}
+
+			// 提取用户消息（去除\n）
+			msg := string(buf[:n-1])
+
+			// 将消息进行广播
+			server.BroadCast(user, msg)
+		}
+	}()
 
 	select {}
 }
