32d05bc70c6c067dbebeeb7666d9ee4bf2966d21
liufenghong <1476212137@qq.com>
2022-05-19 16:40:24 +0800

    补充完Client的公聊和私聊

--- client/client.go
+++ client/client.go
@@ -3,7 +3,9 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"net"
+	"os"
 )
 
 type Client struct {
@@ -35,7 +37,13 @@ func NewClient(serverIp string, serverPort int) *Client {
 	return client
 }
 
-func (client *Client) menu() bool {
+// 处理服务器回应的消息，直接显示到标准输出
+func (client *Client) DealRespone() {
+	// 一旦client.conn有数据，就直接copy到stdout标准输出上, 永久阻塞监听
+	io.Copy(os.Stdout, client.conn)
+}
+
+func (client *Client) Menu() bool {
 	var flag int
 
 	fmt.Println("1.talk to the world")
@@ -54,23 +62,102 @@ func (client *Client) menu() bool {
 	}
 }
 
-func (cliet *Client) Run() {
-	for cliet.flag != 0 {
-		for !cliet.menu() {
+//查询在线用户
+func (client *Client) SelectUsers() {
+	_, err := client.conn.Write([]byte("who\n"))
+	if err != nil {
+		fmt.Println("select users err: ", err)
+	}
+}
+
+//私聊模式
+func (client *Client) PrivateChat() {
+	var remoteName string
+	var msg string
+	client.SelectUsers()
+	fmt.Println("chat to someone please input remote name:(exit will exit)")
+	fmt.Scanln(&remoteName)
+
+	for remoteName != "exit" {
+		fmt.Println("chat to someone please input your message:(exit will exit)")
+		fmt.Scanln(&msg)
+
+		if len(remoteName) != 0 {
+			for msg != "exit" {
+				if len(msg) != 0 {
+					sendMsg := "to|" + remoteName + "|" + msg + "\n"
+					_, err := client.conn.Write([]byte(sendMsg))
+					if err != nil {
+						fmt.Println("private chat err: ", err)
+						break
+					}
+				}
+
+				fmt.Println("chat to someone please input your message:(exit will exit)")
+				msg = ""
+				fmt.Scanln(&msg)
+			}
+
+			client.SelectUsers()
+			fmt.Println("chat to someone please input remote name:(exit will exit)")
+			remoteName = ""
+			fmt.Scanln(&remoteName)
+		}
+	}
+}
+
+// 公聊
+func (client *Client) PublicChat() {
+	fmt.Println("chat to world please input your message:(exit will exit)")
+	var msg string
+	fmt.Scanln(&msg)
+
+	for msg != "exit" {
+		if len(msg) != 0 {
+			sendMsg := msg + "\n"
+			_, err := client.conn.Write([]byte(sendMsg))
+			if err != nil {
+				fmt.Println("public chat err: ", err)
+				break
+			}
+		}
+
+		fmt.Println("chat to world please input your message:(exit will exit)")
+		msg = ""
+		fmt.Scanln(&msg)
+	}
+}
+
+func (client *Client) UpdateName() bool {
+	fmt.Println("please input your new name:")
+	fmt.Scanln(&client.Name)
+
+	sendMsg := "rename|" + client.Name + "\n"
+	_, err := client.conn.Write([]byte(sendMsg))
+	if err != nil {
+		fmt.Println("client.conn.Write err: ", err)
+		return false
+	}
+	return true
+}
+
+func (client *Client) Run() {
+	for client.flag != 0 {
+		for !client.Menu() {
 
 		}
 
 		// 根据不同模式处理不同业务
-		switch cliet.flag {
+		switch client.flag {
 		case 1:
 			// 公聊
-			fmt.Println("choose talk to the world")
+			client.PublicChat()
 		case 2:
 			// 私聊
-			fmt.Println("choose talk to someone")
+			client.PrivateChat()
 		case 3:
 			// 更新用户名
-			fmt.Println("choose rename")
+			client.UpdateName()
 		}
 	}
 }
@@ -94,6 +181,9 @@ func main() {
 		fmt.Println("conn to server fail")
 		return
 	}
+
+	// 开启一个goroutine监听服务器回应
+	go client.DealRespone()
 
 	fmt.Println("conn to server success")
 
